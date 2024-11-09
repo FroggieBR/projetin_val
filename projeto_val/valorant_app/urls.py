5604d932e913ea9a1bc13be0dbc230a14cac4b96
FroggieBR <andleychagas2@gmail.com>
2024-11-08 23:48:20 -0300

    Revert "first commit"
    This reverts commit cc1c8ac82b8536632effb4c9a8f6a7c661efed16.

--- projeto_val/valorant_app/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import PlayerViewSet, TeamViewSet, TournamentViewSet
-
-router = DefaultRouter()
-router.register(r'player', PlayerViewSet, 'player')
-router.register(r'team', TeamViewSet, 'team')
-router.register(r'tournament', TournamentViewSet, 'tournament')
-
-urlpatterns = router.urls
